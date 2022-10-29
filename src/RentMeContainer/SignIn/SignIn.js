54120a2fb0c1ed1724152a79371ec418b79d7ab2
Minor Project <112385553+Minorproject01@users.noreply.github.com>
2022-10-29 13:28:06 +0530

    Update SignIn.js

---
+++ src/RentMeContainer/SignIn/SignIn.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SignIn = () =>{
+    return (
+        <div>
+            <h1>complete here the html part of signin page</h1>
+        </div>
+    )
+}
+export default SignIn;
+
+
+
