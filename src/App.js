862feb609dc3384838197220e020ddd42306a74e
Minor Project <112385553+Minorproject01@users.noreply.github.com>
2022-10-29 13:29:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Card from './RentMeContainer/Card/Card';
+import Home from './RentMeContainer/Home/Home';
+import Footer from './RentMeContainer/Footer/Footer'
+import SignIn from './RentMeContainer/SignIn/SignIn';
+import SignUp from './RentMeContainer/SignUP/SignUp';
 function App() {
   return (
     <div className="App">
-      <p>sgfuishro</p>
+      <Card/>
       hello
+      <Home/>
+      <Footer/>
+      <SignIn/>
+      <SignUp/>
     </div>
   );
 }
