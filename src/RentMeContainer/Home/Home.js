90d7ebd5c1bcb84ca26e047b733a6cb47abd6ef7
Minor Project <112385553+Minorproject01@users.noreply.github.com>
2022-10-29 13:27:35 +0530

    Update Home.js

--- src/RentMeContainer/Home/Home.js
+++ src/RentMeContainer/Home/Home.js
@@ -1,5 +1,17 @@
 import React from "react";
 
+const Home = () =>{
+    return (
+        <div>
+            <h1>complete here the html part og=f home page</h1>
+        </div>
+    )
+}
+export default Home;
+
+
+
+
 
 
 
